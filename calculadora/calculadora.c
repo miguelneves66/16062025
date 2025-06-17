34015bf748e34d62404f9803b264fe3f74b1566f
caua-l <caua.nascimento10@etec.sp.gov.br>
2025-06-17 17:16:21 -0300

    Update calculadora.c

--- calculadora/calculadora.c
+++ calculadora/calculadora.c
@@ -6,22 +6,22 @@ float subtracao(float a, float b);
 float multiplicacao(float a, float b);
 float divisao(float a, float b);
 
-void main() {
+int main() {
     float num1, num2, resultado;
     char operacao;
     char continuar;
     printf("Bem-vindo(a) à Calculadora!\n");
-inicio:
-    printf("\nDigite o primeiro número: ");
+Inicio:
+    printf("\nInsira o primeiro número: ");
     scanf("%f", &num1);
-    printf("\nEscolha a operação:\n");
+    printf("\nSelecione uma operação:\n");
     printf("a - Adição\n");
     printf("s - Subtração\n");
     printf("m - Multiplicação\n");
     printf("d - Divisão\n");
     printf("Operação: ");
     scanf(" %c", &operacao);
-    printf("\nDigite o segundo número: ");
+    printf("\Digite o segundo número: ");
     scanf("%f", &num2);
     if (operacao == 'a') {
         resultado = soma(num1, num2);
@@ -36,9 +36,9 @@ inicio:
         resultado = divisao(num1, num2);
         printf("\nResultado: %f\n", resultado);
     } else {
-        printf("\nOperação inválida!\n");
+        printf("\nOperação inválida!, Coloque um valor válido\n");
     }
-    printf("\nDeseja fazer outro cálculo? (Digite 'q' para sair ou qualquer outra tecla para continuar): ");
+    printf("\nDeseja fazer mais um cálculo? (Digite 'q' para sair ou qualquer outra tecla para continuar): ");
     scanf(" %c", &continuar);
 
     if (continuar == 'q') {
